1bb8252dec618ba971509e5c9f76f0f731371daf
hieppie <hiepqduong@gmail.com>
2022-04-11 22:23:28 -0400

    init tx model

---
+++ app/models/treatment.js
@@ -0,0 +1,37 @@
+// Mongoose is a ODM ( Object-Docment Mapper). good at converting Objects (Js) and Documents (MongoDb). stored as BSON (similar to JSON)
+const mongoose = require('mongoose')
+
+// schema for tx
+const treatmentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  toothNumber: {
+    type: Number,
+    required: true
+  },
+  radiograph: {
+    type: String,
+    required: true
+  },
+  dateOfService: {
+    type: String,
+    required: true
+  },
+  // relationship between Users and Events using references (not subDOC)
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true,
+  // toObject is the function that runs when Mongoose is converting from a MongoDB document to a JS object (retrieving from DB)
+  toObject: {},
+  // toJSON is the function that runs when Mongoose is converting from a JS object to a MongoDB document (saving to DB)
+  toJSON: {}
+})
+
+// export, converting the schema to a model
+module.exports = mongoose.model('Treatment', treatmentSchema)
